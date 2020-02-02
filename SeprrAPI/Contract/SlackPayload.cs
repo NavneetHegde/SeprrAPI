3ffbdb449b06b87626b196c39fda42f0bb2f3a3f
Navneet Hegde <navneet.hegde@hotmail.com>
2020-02-02 12:54:52 -0500

    Initial checkin

---
+++ SeprrAPI/Contract/SlackPayload.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace SeprrAPI.Contract
+{
+    public class SlackRequest
+    {
+        public string text { get; set; }
+    }
+    public class SlackResponse
+    {
+        public string text { get; set; }
+        public List<Attachments> attachments { get; set; }
+    }
+
+    public class Attachments
+    {
+        public string title { get; set; }
+        public string text { get; set; }
+        public string color { get; set; }
+        public string[] mrkdwn_in { get; set; } = { "text", "fields" };
+        public List<Fields> fields { get; set; }
+        public string ts { get; set; }
+        public string footer { get; set; }
+    }
+
+    public class Fields
+    {
+        public string title { get; set; }
+        public string value { get; set; }
+    }
+
+    public class ApiResponse
+    {
+        public string orig_train { get; set; }
+        public string orig_departure_time { get; set; }
+        public string orig_line { get; set; }
+        public string orig_arrival_time { get; set; }
+        public string orig_delay { get; set; }
+        public string isdirect { get; set; }
+    }
+}
